591ba11715ccdaa0fb5519838818b44a31566a91
LautaroDillon <134712364+LautaroDillon@users.noreply.github.com>
2024-11-22 13:57:55 -0300

    Loot y plataforma

--- Assets/Jar.cs
+++ Assets/Jar.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Jar : MonoBehaviour
 {
     Guns guns;
+    public LootBag Bag;
 
     private void Start()
     {
         guns = GameObject.Find("Gun").GetComponent<Guns>();
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Bullet")
+        {
             guns.bulletsLeft += 1;
+            Bag.intanceLoot(gameObject.transform.position);
+        }
     }
 }

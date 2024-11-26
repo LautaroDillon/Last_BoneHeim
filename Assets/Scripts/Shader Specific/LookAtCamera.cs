d6203beb4b1326e67760078bfa38f9520957b168
apaletta <agustin.paletta@davinci.edu.ar>
2024-11-26 18:03:06 -0300

    Escudos (En Progreso)

---
+++ Assets/Scripts/Shader Specific/LookAtCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    Camera _cam;
+
+    private void Start()
+    {
+        _cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var dir = _cam.transform.position - transform.position;
+        transform.forward = dir;
+    }
+}

dd7770db056b556d627106d7afbb70a40520d44e
LautaroDillon <134712364+LautaroDillon@users.noreply.github.com>
2024-12-03 16:00:18 -0300

    nuevos enemigos y arreglos

---
+++ Assets/Scripts/Trampas/Chest.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public enum ChestType { Normal, Cursed }
+    public ChestType chestType;
+
+    [Header("Rewards")]
+    public GameObject rewardPrefab;
+    public Transform rewardSpawnPoint;
+
+    [Header("Cursed Effects")]
+    public GameObject cursedEffectPrefab;
+    public GameObject enemyPrefab;
+    public GameObject explosion;
+    public Transform spawnPoint;
+
+    //public float curseEffectDuration = 5f;
+    private bool isOpened = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isOpened)
+        {
+            isOpened = true;
+            if (chestType == ChestType.Normal)
+            {
+                OpenNormalChest();
+            }
+            else
+            {
+                ActivateCursedChest();
+            }
+        }
+    }
+
+    private void OpenNormalChest()
+    {
+        Debug.Log("Cofre abierto: recompensa obtenida");
+        Instantiate(rewardPrefab, rewardSpawnPoint.position, Quaternion.identity);
+        Destroy(gameObject); // Elimina el cofre tras abrirlo
+    }
+
+    private void ActivateCursedChest()
+    {
+        Debug.Log("¡Cofre maldito activado!");
+
+        // Mostrar efecto de maldición
+        if (cursedEffectPrefab != null)
+        {
+            Instantiate(cursedEffectPrefab, transform.position, Quaternion.identity);
+        }
+
+        // Activar un efecto maldito aleatorio
+        int effect = Random.Range(0, 2);
+        switch (effect)
+        {
+            case 0:
+                InvokeEnemies();
+                break;
+            case 1:
+                TriggerExplosion();
+                break;
+            case 2:
+               // ApplyCurseToPlayer();
+                break;
+        }
+
+        Destroy(gameObject, 2f); // Elimina el cofre después de un tiempo
+    }
+
+    private void InvokeEnemies()
+    {
+        Debug.Log("Invocando enemigos...");
+        Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+    }
+
+    private void TriggerExplosion()
+    {
+        Debug.Log("Explosión activada");
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(this.gameObject, 0.1f);
+        // Añadir lógica para una explosión (ejemplo: daño en área)
+    }
+
+    /*private void ApplyCurseToPlayer()
+    {
+        Debug.Log("Maldición aplicada al jugador");
+        // Reduce temporalmente las habilidades del jugador
+        PlayerStats player = GameManager.instance.thisIsPlayer.GetComponent<PlayerStats>();
+        if (player != null)
+        {
+            player.ApplyCurse(curseEffectDuration);
+        }
+    }*/
+}

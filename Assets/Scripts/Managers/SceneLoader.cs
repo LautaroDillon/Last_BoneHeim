0d2993adcc3010623ebff673daea869e626a9f30
LautaroDillon <134712364+LautaroDillon@users.noreply.github.com>
2024-12-06 15:05:24 -0300

    laLALAJFOHDUSDNFG

--- Assets/Scripts/Managers/SceneLoader.cs
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -9,14 +9,7 @@ public class SceneLoader : MonoBehaviour
     public InventoryObject equipment;
     public void LoadScene(int level)
     {
-        FullscreenShader.instance.acidShaderEnabled = false;
-        FullscreenShader.instance.armShaderEnabled = false;
-        FullscreenShader.instance.blazingShaderEnabled = false;
-        FullscreenShader.instance.cursedShaderEnabled = false;
-        FullscreenShader.instance.normalShaderEnabled = false;
-        FullscreenShader.instance.speedShaderEnabled = false;
-        FullscreenShader.instance.vengefulShaderEnabled = false;
-        FullscreenShader.instance.blessedShaderEnabled = false;
+
         SceneManager.LoadScene(level);
         inventory.Clear();
         equipment.Clear();

67395056fcf8c4bbf0232ddb084c688fcf3e4985
Lautaro <134712364+LautaroDillon@users.noreply.github.com>
2025-06-22 17:10:01 -0300

    Add organ descriptions and UI for inventory details

--- Assets/CHRISTEST/Scripts/UI_Inventory/OrganSlotUI.cs
+++ Assets/CHRISTEST/Scripts/UI_Inventory/OrganSlotUI.cs
@@ -3,24 +3,46 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class OrganSlotUI : MonoBehaviour
 {
     public Image iconImage;
     public TextMeshProUGUI nameText;
+    public TextMeshProUGUI descriptionText;
 
     private ItemType organType;
 
     public void SetOrgan(ItemType type)
     {
         organType = type;
-        nameText.text = type.ToString();
-        // iconImage.sprite = tu sprite si tienes uno por tipo
+
+        foreach (var organ in HotbarPlayer.Instance.dataOrgans)
+        {
+            if (organ.type == type)
+            {
+                iconImage.sprite = organ.icon;
+                nameText.text = organ.organName;
+                break;
+            }
+        }
     }
 
-    public void OnRemoveClicked()
+    public void BTN_show()
     {
-        HotbarPlayer.Instance.RemoveToHotbar(organType);
-        OrganInventoryUI.Instance.RefreshInventory(HotbarPlayer.Instance.hotbarlist);
+        OrganInventoryUI.Instance.ShowDescription(organType);
+
+    }
+
+    // Cuando el mouse entra en el slot
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OrganInventoryUI.Instance.ShowDescription(organType);
+    }
+
+    // Cuando el mouse sale
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OrganInventoryUI.Instance.hide();
     }
 }
